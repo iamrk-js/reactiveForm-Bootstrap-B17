55b1e33698d60e3f7aa4a6f6cd445a6209f1941e
iamrk-js <88290009+iamrk-js@users.noreply.github.com>
2025-07-20 21:34:38 +0530

    Reactive Form Bootstrap B17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { CustomRegex } from './shared/const/validatorsPattern'
+import { NoSpaceBarValidator } from './shared/validators/noSpaceValidator'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'reactiveForm';
+export class AppComponent implements OnInit {
+  title = 'reactiveForm'
+  signUpForm!: FormGroup // undefined
+  constructor () {}
+
+  ngOnInit (): void {
+    this.signUpForm = new FormGroup({
+      userName: new FormControl(null, 
+        [
+        Validators.required,
+        Validators.pattern(CustomRegex.username),
+        Validators.minLength(5),
+        Validators.maxLength(8),
+        NoSpaceBarValidator.noSpace
+        ]),
+      email: new FormControl(null, [Validators.required])
+    })
+  }
+
+  onSignUp () {
+    console.log(this.signUpForm)
+  }
+
+  get f(){
+    return this.signUpForm.controls
+  }
+
+  public get userName () {
+    return this.signUpForm.get('userName') as FormControl
+  }
 }
